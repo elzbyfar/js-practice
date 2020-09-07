993100d630e30001c203c411d941caac2344db6b
Luis Alejo <lou.m.alejo@gmail.com>
2020-09-07 02:18:45 -0500

    array packing

---
+++ arrayPacking.js
@@ -0,0 +1,51 @@
+// You are given an array of up to four non-negative integers, each less than 256.
+
+// Your task is to pack these integers into one number M in the following way:
+
+// The first element of the array occupies the first 8 bits of M;
+// The second element occupies next 8 bits, and so on.
+// Return the obtained integer M.
+
+// Note: the phrase "first bits of M" refers to the least significant bits of M - the right-most bits of an integer. For further clarification see the following example.
+
+// Example
+
+// For a = [24, 85, 0], the output should be
+// arrayPacking(a) = 21784.
+
+// An array [24, 85, 0] looks like [00011000, 01010101, 00000000] in binary.
+// After packing these into one number we get 00000000 01010101 00011000 (spaces are placed for convenience), which equals to 21784.
+
+// 00000000 01010101 00011000
+
+// 1 + 4 + 16 + 64 + 1024 + 2048
+
+function arrayPacking(a) {
+  let M = 0;
+
+  const converter = (n) => {
+    const binArray = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    for (let i = 0, e = 8; i <= 8; i++, e--) {
+      if (2 ** e <= n) {
+        binArray[i - 1] = 1;
+        n -= 2 ** e;
+      }
+    }
+
+    return binArray;
+  };
+
+  let exp = 0;
+  for (let n = 0; n < a.length; n++) {
+    let reversedArray = converter(a[n]).reverse();
+    for (let j = 0; j < 8; j++) {
+      M += reversedArray[j] * 2 ** exp;
+      exp++;
+    }
+  }
+
+  return M;
+}
+
+console.log(arrayPacking([24, 85, 0]));
